816bbe23d5fd3a51034c8c86c7e06906b2bc206a
zhanghu <425015654@qq.com>
2018-11-13 20:57:23 +0800

    先随便加点东西

---
+++ test001/src/main/java/com/zh/ctroller/Test01.java
@@ -0,0 +1,9 @@
+package com.zh.ctroller;
+
+public class Test01 {
+	
+	public void getTest(){
+		System.out.println("sssss");
+	}
+
+}
---
+++ test001/src/main/java/com/zh/ctroller/TestMain.java
@@ -0,0 +1,13 @@
+package com.zh.ctroller;
+
+public class TestMain {
+	
+    
+	Test01 test01;
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
